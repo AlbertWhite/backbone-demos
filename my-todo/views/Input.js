faf9bfd0ac873f50bfc65deecd885be76183abeb
Yuebai XU <y.xu@gop.link>
2017-12-27 17:34:54 +0100

    update view

--- my-todo/views/Input.js
+++ my-todo/views/Input.js
@@ -1,5 +1,6 @@
+//in view, render(), initialize, events, initialize className, tagName, id, el(find existing element), attributes
+
 app.InputView = Backbone.View.extend({
-	el: '.input-div',
 	initialize: function(){
 		this.render(); //if it is binded to a DOM element, render while initializing
 	},
@@ -17,4 +18,24 @@ app.InputView = Backbone.View.extend({
 	}
 })
 
-app.inputView = new app.InputView();
+
+app.AddButtonView = Backbone.View.extend({
+	tagName: 'button',
+	initialize: function(){
+		this.render();
+	},
+	render: function(){
+		this.$el.html('add'); //if empty div, append or give html content
+	}
+
+})
+
+//pass model, collection, el, id, className, tagName.. as params
+app.inputView = new app.InputView({
+	el: '.input-div',
+});
+
+app.addButtonView = new app.AddButtonView();
+
+//create view and append to existing view dynamaically
+app.inputView.$el.append(app.addButtonView.el);
